07c202d9ecf5904df410e759ab17d989be0ac3e1
SrMatdroid <sr.matdroid@outlook.com>
2026-07-05 16:58:03 +0200

    Magisk v2.0: fix should_kill flag not being reset for regular apps, add onLoad() detection via prctl

--- MagiskZygisk/source/noappzygote.cpp
+++ MagiskZygisk/source/noappzygote.cpp
@@ -1,4 +1,7 @@
 #include <unistd.h>
+#include <fcntl.h>
+#include <cstring>
+#include <sys/prctl.h>
 #include <android/log.h>
 #include "zygisk.hpp"
 
@@ -9,6 +12,13 @@ public:
     void onLoad(zygisk::Api *api, JNIEnv *env) override {
         (void)api;
         this->env = env;
+
+        char name[16] = {};
+        prctl(PR_GET_NAME, name);
+        if (strncmp(name, "app_zygote", 10) == 0) {
+            LOGD("onLoad: detected app_zygote via prctl, exiting");
+            _exit(0);
+        }
     }
 
     void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
@@ -16,19 +26,20 @@ public:
             should_kill = true;
             if (args->nice_name) {
                 const char *name = env->GetStringUTFChars(args->nice_name, nullptr);
-                LOGD("Blocking app_zygote spawn for %s", name);
+                LOGD("preAppSpecialize: blocking app_zygote for %s", name);
                 env->ReleaseStringUTFChars(args->nice_name, name);
             } else {
-                LOGD("Blocking app_zygote spawn");
+                LOGD("preAppSpecialize: blocking app_zygote");
             }
+        } else {
+            should_kill = false;
         }
     }
 
     void postAppSpecialize(const zygisk::AppSpecializeArgs *args) override {
         (void)args;
         if (should_kill) {
-            should_kill = false;
-            LOGD("Killing app_zygote process");
+            LOGD("postAppSpecialize: killing app_zygote");
             _exit(0);
         }
     }
